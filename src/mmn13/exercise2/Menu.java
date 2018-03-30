24e0b3a5dfb1b8ceaddce3fa280e6c1110a7a132
matific-daniel <daniel@slatescience.com>
2018-03-30 18:04:37 +0300

    mmn13 exercise2 wip

--- src/mmn13/exercise2/Menu.java
+++ src/mmn13/exercise2/Menu.java
@@ -3,31 +3,35 @@ package mmn13.exercise2;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 
 @SuppressWarnings("serial")
 public class Menu extends JPanel implements ActionListener {
-	
+
 	private ArrayList<Item> items;
 	private JTabbedPane menuTabs;
 	private JButton orderButton;
 	private MenuPage firstCourse, mainCourse, dessert, drinks;
 	private JScrollPane firstCourse_scrollable, mainCourse_scrollable, dessert_scrollable, drinks_scrollable;
-	
+	private Order order;
+
 	public Menu(File menu) {
 		super(new BorderLayout());
-		
+
 		items = new ArrayList<Item>();
 		convertFileToItemList(menu);
-		
+
 		firstCourse = new MenuPage(Category.FIRST_COURSE, items);
 		mainCourse = new MenuPage(Category.MAIN_COURSE, items);
 		dessert = new MenuPage(Category.DESSERT, items);
@@ -36,19 +40,22 @@ public class Menu extends JPanel implements ActionListener {
 		mainCourse_scrollable = new JScrollPane(mainCourse);
 		dessert_scrollable = new JScrollPane(dessert);
 		drinks_scrollable = new JScrollPane(drinks);
-		
+
 		menuTabs = new JTabbedPane();
 		menuTabs.addTab("First Course", firstCourse_scrollable);
 		menuTabs.addTab("Main Course", mainCourse_scrollable);
 		menuTabs.addTab("Dessert", dessert_scrollable);
 		menuTabs.addTab("Drinks", drinks_scrollable);
-		
+
 		orderButton = new JButton("Order");
-		
+		orderButton.addActionListener(this);
+
 		add(menuTabs, BorderLayout.CENTER);
 		add(orderButton, BorderLayout.SOUTH);
+
+		order = new Order();
 	}
-	
+
 	private void convertFileToItemList(File menu) {
 		try {
 			Scanner input = new Scanner(menu);
@@ -60,26 +67,109 @@ public class Menu extends JPanel implements ActionListener {
 				}
 				switch(i % 3) {
 				case 0: items.get(items.size()-1).setDescription(input.nextLine());
-					break;
+				break;
 				case 1: items.get(items.size()-1).setCategory(Category.valueOf(input.nextLine()));
-					break;
+				break;
 				case 2: items.get(items.size()-1).setPrice(Integer.parseInt(input.nextLine()));
-					break;
+				break;
 				}	
 				i++;
 			}
 			input.close();
 		}
 		catch (FileNotFoundException e) {
-			
+
 		}
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == orderButton) {
-			
+			String[] options = {"Confirm", "Change", "Cancel"};
+			int choice = JOptionPane.showOptionDialog(this, order.generateOrder(menuTabs), "Order Details", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+			switch(choice) {
+			case 0: 
+				String outputFileName = JOptionPane.showInputDialog("Please enter your first name and ID(e.g. Name123456789):");
+				File outputFile = new File("D:\\workspace\\advanced-java\\bin\\mmn13\\exercise2\\" + outputFileName + ".txt");
+				BufferedWriter writer = null;
+				try {
+					writer = new BufferedWriter(new FileWriter(outputFile));
+					writer.write(order.getOrderDetails());
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				} finally {
+					try {
+						writer.close();
+					} catch (Exception e2) {
+					}
+				}
+				resetOrder();
+				break;
+			case 1:
+				order = new Order();
+				break;
+			case 2:
+			case -1:
+				resetOrder();
+				break;
+			}
 		}
 	}
 
+	private void resetOrder() {
+		removeAll();
+
+		firstCourse = new MenuPage(Category.FIRST_COURSE, items);
+		mainCourse = new MenuPage(Category.MAIN_COURSE, items);
+		dessert = new MenuPage(Category.DESSERT, items);
+		drinks = new MenuPage(Category.DRINK, items);
+		firstCourse_scrollable = new JScrollPane(firstCourse);
+		mainCourse_scrollable = new JScrollPane(mainCourse);
+		dessert_scrollable = new JScrollPane(dessert);
+		drinks_scrollable = new JScrollPane(drinks);
+
+		menuTabs = new JTabbedPane();
+		menuTabs.addTab("First Course", firstCourse_scrollable);
+		menuTabs.addTab("Main Course", mainCourse_scrollable);
+		menuTabs.addTab("Dessert", dessert_scrollable);
+		menuTabs.addTab("Drinks", drinks_scrollable);
+
+		add(menuTabs, BorderLayout.CENTER);
+		add(orderButton, BorderLayout.SOUTH);
+
+		order = new Order();
+		revalidate();
+		repaint();
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/mmn13/exercise2/Order.java
+++ src/mmn13/exercise2/Order.java
@@ -1,5 +1,49 @@
 package mmn13.exercise2;
 
-public class Order {
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
 
+public class Order {
+	
+	private int totalPrice;
+	private String orderDetails;
+	private final int PRICE = 1;
+	
+	public Order() {
+		totalPrice = 0;
+		orderDetails = "";
+	}
+	
+	public String getOrderDetails() {
+		return orderDetails;
+	}
+	
+	public String generateOrder(JTabbedPane menuTabs) {
+		for(int i = 0; i < menuTabs.getComponentCount(); i++) {
+			MenuPage menuPage = (MenuPage) ((JScrollPane) menuTabs.getComponent(i)).getViewport().getView();
+			orderDetails += menuTabs.getTitleAt(i) + "\r\n===============\r\n" + extractSelectedFromMenuPage(menuPage) + "\r\n";
+		}
+		orderDetails += "\r\nTOTAL PRICE: " + totalPrice;
+		return orderDetails;
+	}
+	
+	@SuppressWarnings("unchecked")
+	private String extractSelectedFromMenuPage(MenuPage menuPage) {
+		String extractedItems = "";
+		for(int i = 0; i < menuPage.getComponentCount(); i++) {
+			JLabel description = (JLabel) ((JPanel) menuPage.getComponent(i)).getComponent(0);
+			JCheckBox isChosen = (JCheckBox) ((JPanel) menuPage.getComponent(i)).getComponent(1);
+			JComboBox<Integer> amount = (JComboBox<Integer>) ((JPanel) menuPage.getComponent(i)).getComponent(2);
+			if(isChosen.isSelected()) {
+				int amountTimesPrice = (Integer.parseInt(description.getText().split(": ")[PRICE])) * (Integer)amount.getSelectedItem();
+				extractedItems += description.getText() + " * " + amount.getSelectedItem() + " = " + amountTimesPrice + "\r\n";
+				totalPrice += amountTimesPrice;
+			}
+		}
+		return extractedItems;
+	}
 }
