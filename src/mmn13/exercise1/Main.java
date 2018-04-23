9cea177c8019e08c2db35e5f706f92382ccb589f
Daniel Lewinski <daniel@slatescience.com>
2018-04-23 09:56:17 +0300

    mmn13 exercise1 removed unnecessary run method

--- src/mmn13/exercise1/Main.java
+++ src/mmn13/exercise1/Main.java
@@ -7,15 +7,11 @@ import javax.swing.JFrame;
 public class Main {
 
 	public static void main(String[] args){
-		java.awt.EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				JFrame frame = new JFrame("MyPaint");
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.setPreferredSize(new Dimension(300,400));
-				frame.add(new MainPanel());
-				frame.pack();
-				frame.setVisible(true);
-			}
-		});  
+		JFrame frame = new JFrame("MyPaint");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setPreferredSize(new Dimension(300,400));
+		frame.add(new MainPanel());
+		frame.pack();
+		frame.setVisible(true);
 	} 
 }
