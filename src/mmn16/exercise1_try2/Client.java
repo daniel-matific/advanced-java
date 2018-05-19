3ee0960adf086c5e97b872ba3aa4fba663b1d253
matific-daniel <daniel@slatescience.com>
2018-05-19 08:45:06 +0300

    mmn16 exercise1 mostly done

--- src/mmn16/exercise1_try2/Client.java
+++ src/mmn16/exercise1_try2/Client.java
@@ -2,7 +2,10 @@ package mmn16.exercise1_try2;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.concurrent.CountDownLatch;
--- src/mmn16/exercise1_try2/ServerThread.java
+++ src/mmn16/exercise1_try2/ServerThread.java
@@ -1,6 +1,9 @@
 package mmn16.exercise1_try2;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.concurrent.CountDownLatch;
 
